f954ef2484ad739f36a4d1e3b4aa8bb6fc904358
exequiel <conan34681335@gmail.com>
2023-06-30 19:54:36 -0300

    cambios

--- src/app/servicios/portafolio.service.ts
+++ src/app/servicios/portafolio.service.ts
@@ -2,64 +2,42 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
-import { Usuario } from 'src/interfaces/Usuario'
-
-
+import { Usuario } from 'src/interfaces/Usuario';
 
 @Injectable({
   providedIn: 'root'
 })
-
-
-
 export class PortafolioService {
- 
-  
   private apiURL = environment.apiURL;
 
   constructor(private http: HttpClient) {}
 
-  getProyecto() {
-    const url1 = `${this.apiURL}/ver/proyecto`;
-    return this.http.get(url1);
-    
+  getProyecto(): Observable<any> {
+    return this.http.get(`${this.apiURL}/ver/proyecto`);
   }
-  getBootcamp() {
-    const url2 = `${this.apiURL}/ver/experiencia`;
-    return this.http.get(url2);
-    
+
+  getBootcamp(): Observable<any> {
+    return this.http.get(`${this.apiURL}/ver/experiencia`);
   }
-  getEducacion() {
-    const url3 = `${this.apiURL}/ver/educacion`;
-    return this.http.get(url3);
-    
+
+  getEducacion(): Observable<any> {
+    return this.http.get(`${this.apiURL}/ver/educacion`);
   }
-  getPerfil() {
-    const url4 = `${this.apiURL}/ver/usuario`;
-    return this.http.get(url4);
-    
+
+  getPerfil(): Observable<any> {
+    return this.http.get(`${this.apiURL}/ver/usuario`);
   }
-  getEmpleo() {
-    const url5 = `${this.apiURL}/ver/empleo`;
-    return this.http.get(url5);
-    
+
+  getEmpleo(): Observable<any> {
+    return this.http.get(`${this.apiURL}/ver/empleo`);
   }
 
   actualizarDescripcion(usuario: Usuario): Observable<any> {
-    const url6 = `${this.apiURL}/nueva/descripcion`;
-    return this.http.post(url6, usuario);
+    return this.http.post(`${this.apiURL}/nueva/descripcion`, usuario);
   }
-  
+
   login(credentials: { email: string, password: string }): Observable<any> {
-    const url7 = `${this.apiURL}/login`;
-    return this.http.post(url7, credentials);
+    return this.http.post(`${this.apiURL}/login`, credentials);
   }
-  getUsuario() {
-    
-  }
-  
-
-
-  
 
 }
